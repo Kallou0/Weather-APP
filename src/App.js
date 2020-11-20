0584fada61269eb28abce3575f7ed8fe7d3fb25a
Marshal Nzenza <mnzenza@zimswitch.co.zw>
2020-11-20 14:40:28 +0200

    fetching data

--- src/App.js
+++ src/App.js
@@ -11,6 +11,48 @@ class App extends React.Component {
     }
   }
 
+  getWeatherData = () => {
+    const weatherApi = `http://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=${process.env.REACT_APP_WEATHER_KEY}`;
+  
+    fetch(weatherApi, { signal: this.controllerSignal })
+    .then(response => response.json())
+    .then(
+      (result) => {
+  
+        console.log(result);
+  
+        const { name } = result;
+        const { country } = result.sys;
+        const { temp, temp_min, temp_max, feels_like, humidity } = result.main;
+        const { description, icon } = result.weather[0];
+        const { speed, deg } = result.wind;
+  
+        this.setState({
+          isLoaded: true,
+          weatherData: {
+            name,
+            country,
+            description,
+            icon,
+            temp: temp.toFixed(1),
+            feels_like: feels_like.toFixed(1),
+            temp_min: temp_min.toFixed(1),
+            temp_max: temp_max.toFixed(1),
+            speed,
+            deg,
+            humidity
+          }
+        });
+      },
+      (error) => {
+        this.setState({
+          isLoaded: true,
+          error
+        });
+      }
+    );
+  }
+
   render() {
     return (
       <div className='App'>
