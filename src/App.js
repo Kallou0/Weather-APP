1c3b10fc0a36cf8a083aa274684c0ad8d073d14e
Marshal Nzenza <mnzenza@zimswitch.co.zw>
2020-11-20 14:05:44 +0200

    changing css to sass

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
+import Forecast from "./components/Forecast/Forecast";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-      <main>
-        
-      </main>
-      <footer>Page created by</footer>
-    </div>
-  );
+ return (
+   <div className="App">
+     <header className="App-header">
+       <h1>React Weather App</h1>
+     </header>
+     <main>
+       <Forecast />
+     </main>
+     <footer>
+       Page created by Marshal Kallou Nzenza
+     </footer>
+   </div>
+ );
 }
 
 export default App;
